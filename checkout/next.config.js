092fe5e3e8421be8c034e93a9b988b0a8a7e303e
Abhiman Ranaweera <abhiman.max@gmail.com>
2024-03-27 08:15:20 +0530

    feat: add configs

---
+++ checkout/next.config.js
@@ -0,0 +1,32 @@
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+// this enables you to use import() and the webpack parser
+// loading remotes on demand, not ideal for SSR
+const remotes = isServer => {
+  const location = isServer ? 'ssr' : 'chunks';
+  return {
+    home: `home@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+    shop: `shop@http://localhost:3002/_next/static/${location}/remoteEntry.js`,
+  };
+};
+module.exports = {
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'checkout',
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './title': './components/exposedTitle.js',
+          './checkout': './pages/checkout.js',
+          './pages-map': './pages-map.js',
+        },
+        remotes: remotes(options.isServer),
+        shared: {},
+        extraOptions:{
+          exposePages: true
+        }
+      }),
+    );
+
+    return config;
+  },
+};
